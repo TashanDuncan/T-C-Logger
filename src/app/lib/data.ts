7fcce894c1b9c96edb2ece04d5e44733248313a2
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-12-23 20:25:09 +0000

    Add tags and user ratings to item list

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -5,15 +5,24 @@ import { unstable_noStore as noStore } from "next/cache";
 const prisma = new PrismaClient();
 
 export async function fetchItems() {
+  noStore();
   try {
-    const items = await prisma.items.findMany();
+    const items = await prisma.items.findMany({
+      include: {
+        tags: true,
+        user_items: {
+         where: { OR: [{ user_id: 1 }, { user_id: 2 }] },
+        },
+      },
+    });
+
     console.log(items);
+    console.log(items[0].user_items);
     await prisma.$disconnect();
     return items;
   } catch (error) {
-    console.error('Database Error:', error);
+    console.error("Database Error:", error);
     await prisma.$disconnect();
-    throw new Error('Failed to fetch item data.');
+    throw new Error("Failed to fetch item data.");
   }
-
 }
--- src/app/logger/(overview)/page.tsx
+++ src/app/logger/(overview)/page.tsx
@@ -1,4 +1,5 @@
 import { fetchItems } from "@/app/lib/data";
+import { Badge } from "@/app/ui/components/ui/badge";
 import {
   Table,
   TableBody,
@@ -18,21 +19,33 @@ export default async function Page() {
         <TableRow>
           <TableHead>Name</TableHead>
           <TableHead>Tags</TableHead>
-          <TableHead>Consumed</TableHead>
-          <TableHead>Rating</TableHead>
+          <TableHead>Experienced</TableHead>
+          <TableHead>Your Rating</TableHead>
+          <TableHead>Partner Rating</TableHead>
           <TableHead>Actions</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {items.map((item) => (
-          <TableRow key={item.id}>
-            <TableCell>{item.title}</TableCell>
-            <TableCell>{item.tags}</TableCell>
-            <TableCell>{}</TableCell>
-            <TableCell>{}</TableCell>
-            <TableCell>{}</TableCell>
-          </TableRow>
-        ))}
+        {items.map((item) => {
+          const { tags, user_items } = item;
+          const userItem = user_items[0];
+          const partnerItem = user_items[1];
+          return (
+            <TableRow key={item.id}>
+              <TableCell>{item.title}</TableCell>
+              <TableCell>
+                {tags.map((tag) => (
+                  <Badge key={tag.id} className="mx-2">
+                    {tag.name}
+                  </Badge>
+                ))}
+              </TableCell>
+              <TableCell>{}</TableCell>
+              <TableCell>{userItem.rating}</TableCell>
+              <TableCell>{partnerItem.rating}</TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
