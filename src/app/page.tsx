92ed6d6a613bfed4aba92f80daefb33b2b4f4733
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-12-28 08:19:02 +0000

    fix dropdown menu link bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,58 @@
-import Image from 'next/image'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from './ui/components/ui/dropdown-menu'
-import { Button } from './ui/components/ui/button'
-import Link from 'next/link'
-import { fetchItemTypes } from './lib/data'
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/components/ui/dropdown-menu";
+import { Button } from "./ui/components/ui/button";
+import Link from "next/link";
+import { fetchItemTypes } from "./lib/data";
 
 export default async function Home() {
-  const itemTypes = await fetchItemTypes()
+  const itemTypes = await fetchItemTypes();
   return (
     <main className="flex min-h-screen items-center flex-col p-24">
-       <div className="flex justify-center items-center">
-      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <text x="0" y="50" className="text-white text-lg">
-          <textPath href="#curve" className="fill-current">
-            Tashan&apos;s + Christina&apos;s Favorites
-          </textPath>
-        </text>
-        <path id="curve" d="M10 80 Q 120 10 240 80" className="stroke-current text-black" />
-      </svg>
-    </div>
+      <div className="flex justify-center items-center">
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+          <text x="0" y="50" className="text-white text-lg">
+            <textPath href="#curve" className="fill-current">
+              Tashan&apos;s + Christina&apos;s Favorites
+            </textPath>
+          </text>
+          <path
+            id="curve"
+            d="M10 80 Q 120 10 240 80"
+            className="stroke-current text-black"
+          />
+        </svg>
+      </div>
       <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">Menu</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Our Favourites</DropdownMenuLabel>
-        <DropdownMenuSeparator/>
-        <DropdownMenuGroup>
-          {itemTypes.map((item, i) => (<DropdownMenuItem key={i}><Link href={item.name}>{item.description}</Link>
-          </DropdownMenuItem>))}
-          <DropdownMenuSeparator/>
-          <DropdownMenuItem>
-            Add New
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Menu</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Our Favourites</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            {itemTypes.map((item, i) => (
+              <Link href={item.name} key={i}>
+                <DropdownMenuItem>{item.description}</DropdownMenuItem>
+              </Link>
+            ))}
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Add New</DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </main>
-  )
+  );
 }
