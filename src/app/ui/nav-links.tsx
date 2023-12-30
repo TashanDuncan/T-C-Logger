9d3b37ab5316fbaeabd2fcb7df3b8bd3670e48ca
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-12-30 12:51:28 +0000

    Remove HomeNavLink component and update NavLinks component

--- src/app/ui/nav-links.tsx
+++ src/app/ui/nav-links.tsx
@@ -1,25 +1,39 @@
 "use client";
 import Link from "next/link";
-import HomeNavLink from "./components/home-nav-link";
-import { Button } from "./components/ui/button";
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
+import { HomeIcon } from "lucide-react";
 
 export default function NavLinks({ links }: { links: JSX.Element[] }) {
-  const [linkList] = useState<any[]>(links);
+  const [linkList] = useState<JSX.Element[]>(links);
   const pathname = usePathname();
   const router = useRouter();
 
   useEffect(() => {
-    linkList.find((link) => {
+    const activeCategory = linkList.find((link) => {
       return `/${link.props.href}` === pathname;
-    }).props.className =
-      "text-blue-600 flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium  hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3";
+    });
+
+    if (activeCategory) {
+      activeCategory.props.className =
+        "text-blue-600 flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium  hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3";
+    }
     router.refresh();
   }, [pathname, linkList, router]);
   return (
     <>
-      <HomeNavLink />
+      <Link
+        href={"/"}
+        className={clsx(
+          "flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium  hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+          {
+            "text-blue-600": pathname === "/",
+          }
+        )}
+      >
+        <HomeIcon />
+      </Link>
       {linkList}
       <Link
         href={"/items/new"}
