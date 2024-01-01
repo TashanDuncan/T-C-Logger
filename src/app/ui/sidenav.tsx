2eaefdf62393da5d58cabf5a510e7ab5df496cb8
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2024-01-01 19:39:38 +0000

    Fix href in SideNav component

--- src/app/ui/sidenav.tsx
+++ src/app/ui/sidenav.tsx
@@ -9,7 +9,7 @@ export default async function SideNav() {
   const itemTypeLinks = res?.map((i) => (
     <Link
       key={i.id}
-      href={i.slug}
+      href={`/${i.slug}`}
       className={
         "flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium  hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
       }
