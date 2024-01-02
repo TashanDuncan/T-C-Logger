9926bc4fde0ad0693ec884f333c4a8d9e327dae1
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2024-01-02 11:24:41 +0000

    Remove unused import in providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,10 +2,8 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { useRouter } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
 
   return (
     <NextThemesProvider attribute="class" defaultTheme="dark">
