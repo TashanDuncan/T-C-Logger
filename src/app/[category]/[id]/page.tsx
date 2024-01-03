23bb74bb6898413129076c4f93debce2e519e80c
viewtifult <tashanduncan1994@gmail.com>
2024-01-03 19:20:00 +0000

    Refactor user review component and add partner review functionality

--- src/app/[category]/[id]/page.tsx
+++ src/app/[category]/[id]/page.tsx
@@ -2,17 +2,22 @@ import { fetchItemById } from "@/app/lib/data";
 import { getRatingValue } from "@/app/lib/utils";
 import RatingOptions from "@/app/ui/components/rating-options";
 import { Badge } from "@/app/ui/components/ui/badge";
-import { Button } from "@/app/ui/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/app/ui/components/ui/card";
-import { Textarea } from "@/app/ui/components/ui/textarea";
+
 import Image from "next/image";
+import UserReview from "./user-review";
+import { Separator } from "@/app/ui/components/ui/separator";
+import { Checkbox } from "@/app/ui/components/ui/checkbox";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/ui/components/ui/select";
 
 export default async function Page({
   params: { id },
@@ -22,6 +27,7 @@ export default async function Page({
   const item = await fetchItemById(parseInt(id));
   const userItem = item?.userItems.find((item) => item.userId === 1);
   const partnerItem = item?.userItems.find((item) => item.userId === 2);
+
   return (
     <>
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
@@ -49,6 +55,7 @@ export default async function Page({
             <CardHeader>
               <CardTitle>Average Rating: {item?.avgRating}</CardTitle>
               <CardDescription className="text-center">
+                Overall you both think {item.title} is{" "}
                 {`${getRatingValue(item?.avgRating).replace(/\(\d+\)\s/, "")}!`}
               </CardDescription>
             </CardHeader>
@@ -60,34 +67,58 @@ export default async function Page({
 
       <div className="flex justify-around flex-wrap">
         <div className="flex flex-col w-[350px] my-6 items-center">
-          <Image src={"/tashan.jpg"} alt="user" width={150} height={150} />
+          <Image
+            src={"/tashan.jpg"}
+            alt="user"
+            width={200}
+            height={200}
+            className="w-[200px] h-[200px]"
+          />
           <h3 className="text-center text-3xl font-bold dark:text-white my-3">
             Your Review
           </h3>
-          {/* <RatingOptions defaultValue={userItem?.rating.toString()} /> */}
-          <Textarea
-            className="w-full"
-            placeholder="Enter your Review"
-            defaultValue={userItem?.review ?? ""}
-          />
-          <Button className="mt-3">Update Review</Button>
+          <UserReview userItem={userItem} />
         </div>
+        <Separator className="md:hidden my-4" />
         <div className="flex flex-col w-[350px] my-6 items-center">
           <Image
             src={"/christina.png"}
             alt="partner"
-            width={150}
-            height={150}
+            width={200}
+            height={200}
+            className="w-[200px] h-[200px]"
           />
           <h3 className="text-center text-3xl font-bold dark:text-white my-3">
             Partner Review
           </h3>
-          {/* <RatingOptions defaultValue={partnerItem?.rating.toString()}  disabled={true}/> */}
-          <Textarea
-            disabled
-            className="w-full"
-            defaultValue={partnerItem?.review ?? "Pending Review"}
-          />
+          <div className="flex flex-col items-center space-x-3 space-y-3 rounded-md  py-4 justify-center">
+            <span className="text-sm font-bold dark:text-white">
+              {partnerItem?.experienced
+                ? `{Partner} has Experienced ${item?.title}`
+                : `${item?.title} has yet to be Experienced by {Partner}`}
+            </span>
+            <Checkbox
+              className="w-6 h-6"
+              checked={partnerItem?.experienced}
+              disabled={true}
+            />
+          </div>
+          <div>
+            <span className="text-sm font-bold dark:text-white">Rating</span>
+            <Select disabled={true}>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue
+                  placeholder={getRatingValue(partnerItem?.rating || 0)}
+                />
+              </SelectTrigger>
+            </Select>
+          </div>
+          <div className="flex flex-col  space-x-3 space-y-3 rounded-md  py-4  w-full">
+            <span className="text-sm font-bold dark:text-white ">Review</span>
+            <p className="mb-3 dark:text-gray-400">
+              {partnerItem?.review ?? "Pending Review"}
+            </p>
+          </div>
         </div>
       </div>
     </>
---
+++ src/app/[category]/[id]/user-review.tsx
@@ -0,0 +1,96 @@
+"use client";
+import RatingOptions from "@/app/ui/components/rating-options";
+import { Button } from "@/app/ui/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/app/ui/components/ui/form";
+import { Textarea } from "@/app/ui/components/ui/textarea";
+import { UserItem } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { UserItemSchema } from "@/app/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Checkbox } from "@/app/ui/components/ui/checkbox";
+import { createOrUpdateUserItem } from "@/app/lib/actions";
+
+async function onSubmit(values: z.infer<typeof UserItemSchema>) {
+  console.log(values);
+  await createOrUpdateUserItem(values);
+}
+
+export default function UserReview({
+  userItem,
+}: {
+  userItem: UserItem | undefined;
+}) {
+  const form = useForm<z.infer<typeof UserItemSchema>>({
+    resolver: zodResolver(UserItemSchema),
+    defaultValues: {
+      id: userItem?.itemId,
+      rating: userItem?.rating,
+      review: userItem?.review ?? undefined,
+      experienced: !!userItem?.experienced,
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <FormField
+          control={form.control}
+          name="experienced"
+          render={({ field }) => (
+            <FormItem className="flex flex-col items-center space-x-3 space-y-3 rounded-md  py-4 justify-center">
+              <FormLabel>Experienced</FormLabel>
+              <FormControl>
+                <Checkbox
+                  className="w-6 h-6"
+                  checked={field.value || false}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="rating"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Rating*</FormLabel>
+              <FormControl>
+                <RatingOptions
+                  defaultValue={field.value?.toString() ?? "0"}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="review"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormLabel>Review*</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Enter your Review" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="mt-3" type="submit">
+          Update
+        </Button>
+      </form>
+    </Form>
+  );
+}
