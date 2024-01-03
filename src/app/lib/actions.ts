2a358f68151749a54c494be0cd9e5a0d32b64f08
viewtifult <tashanduncan1994@gmail.com>
2024-01-03 19:19:39 +0000

    Add createOrUpdateUserItem function to actions.ts

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -2,7 +2,7 @@
 import { PrismaClient } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { CreateItemSchema } from "./utils";
+import { CreateItemSchema, UserItemSchema } from "./utils";
 import { z } from "zod";
 
 const prisma = new PrismaClient();
@@ -64,4 +64,52 @@ export async function createItem(data: z.infer<typeof CreateItemSchema>) {
   }
   revalidatePath(`/${category.slug}`);
   redirect(`/${category.slug}`);
+}
+
+export async function createOrUpdateUserItem(
+  data: z.infer<typeof UserItemSchema>
+) {
+  const validatedData = UserItemSchema.parse(data);
+  const item = await prisma.item.findUnique({
+    where: { id: validatedData.id },
+  });
+  const category = await prisma.itemCategory.findUnique({
+    where: { id: item?.categoryId },
+  });
+  if (!category?.id) {
+    throw new Error("Invalid category.");
+  }
+  try {
+    await prisma.item.update({
+      where: {
+        id: validatedData.id,
+      },
+      data: {
+        userItems: {
+          upsert: {
+            where: { userId_itemId: { userId: 1, itemId: validatedData.id } },
+            create: {
+              rating: validatedData.rating || 0,
+              experienced: validatedData.experienced,
+              review: validatedData.review,
+              users: {},
+            },
+            update: {
+              rating: validatedData.rating || 0,
+              experienced: validatedData.experienced,
+              review: validatedData.review,
+            },
+          },
+        },
+      },
+    });
+    await prisma.$disconnect();
+  } catch (error) {
+    console.error("Database Error:", error);
+    await prisma.$disconnect();
+    throw new Error(`Failed to create data.`);
+  }
+
+  revalidatePath(`/${category?.slug ?? "category"}/${validatedData.id}`);
+  redirect(`/${category?.slug ?? "category"}/${validatedData.id}`);
 }
