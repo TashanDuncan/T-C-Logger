475e9889b6f466661e6391a515b2b4273dd2c0af
viewtifult <tashanduncan1994@gmail.com>
2024-01-04 13:45:37 +0000

    Update image sizes and remove unnecessary links

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,27 +3,21 @@ import Image from "next/image";
 export default function Page() {
   return (
     <>
-      <h1>
-        <a href="./things/main-page.html">
-          Tashan and Christina&apos;s Media Logger!
-        </a>
-      </h1>
+      <h1>Tashan and Christina&apos;s Media Logger!</h1>
 
       <div>
         <h1>Profiles</h1>
         <div>
           <div>
-            <Image src="/tashan.jpg" alt={""} />
+            <Image src="/tashan.jpg" alt={""} width={200} height={200} />
             <span>Tashan</span>
           </div>
           <div>
-            <a href="./things/main-page.html">
-              <Image src="/christina.PNG" alt={""} />
-            </a>
+            <Image src="/christina.png" alt={""} width={200} height={200} />
             <span>Christina</span>
           </div>
           <div>
-            <Image src="/guest.png" alt={""} />
+            <Image src="/guest.png" alt={""} width={200} height={200} />
             <span>Guest</span>
           </div>
         </div>
