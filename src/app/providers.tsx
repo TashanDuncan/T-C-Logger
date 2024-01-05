1b8dd041773ec553e1e251e7053a5c9aabf5fa5c
viewtifult <tashanduncan1994@gmail.com>
2024-01-05 17:01:55 +0000

    Fix async function declaration in RootLayout and remove unused imports in providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,21 +2,16 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
+
 
 export function Providers({
-  session,
   children,
 }: {
-  session: Session | null;
   children: React.ReactNode;
 }) {
   return (
-    <SessionProvider session={session}>
-      <NextThemesProvider attribute="class" defaultTheme="dark">
-        {children}
-      </NextThemesProvider>
-    </SessionProvider>
+    <NextThemesProvider attribute="class" defaultTheme="dark">
+      {children}
+    </NextThemesProvider>
   );
 }
