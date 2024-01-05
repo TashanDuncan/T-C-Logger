923085e56055df58ae2c2dc0753f27d127779e1a
viewtifult <tashanduncan1994@gmail.com>
2024-01-05 17:35:23 +0000

    Update profile selection UI

--- src/app/login/profile-selection.tsx
+++ src/app/login/profile-selection.tsx
@@ -15,16 +15,30 @@ export default function ProfileSelection({
             alt={""}
             width={200}
             height={200}
+            className="w-[200px] h-[200px] cursor-pointer"
             onClick={() => setSelectedProfile("tashan@test.com")}
           />
           <span>Tashan</span>
         </div>
         <div>
-          <Image src="/christina.png" alt={""} width={200} height={200} onClick={() => setSelectedProfile("christina@test.com")} />
+          <Image
+            src="/christina.png"
+            alt={""}
+            width={200}
+            height={200}
+            className="w-[200px] h-[200px] cursor-pointer"
+            onClick={() => setSelectedProfile("christina@test.com")}
+          />
           <span>Christina</span>
         </div>
         <div>
-          <Image src="/guest.png" alt={""} width={200} height={200} />
+          <Image
+            src="/guest.png"
+            alt={""}
+            width={200}
+            height={200}
+            className="w-[200px] h-[200px]"
+          />
           <span>Guest</span>
         </div>
       </div>
