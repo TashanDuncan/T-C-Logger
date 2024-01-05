570a10830ce3c6a4020499d554b1bb32b39d9dbd
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2024-01-05 11:47:31 +0000

    Add login form component

---
+++ src/app/login/login-form.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useFormState, useFormStatus } from "react-dom";
+import { authenticate } from "@/app/lib/actions";
+import { ArrowRightIcon, KeyIcon } from "lucide-react";
+import { Button } from "../ui/components/ui/button";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/components/ui/form";
+import { Input } from "../ui/components/ui/input";
+
+async function onSubmit(values: any) {
+  console.log(values);
+  await authenticate({ email: "tashan@test.com", ...values });
+}
+
+export default function LoginForm() {
+  const form = useForm({});
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter password..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button className="mt-4 w-full" aria-disabled={pending}>
+      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    </Button>
+  );
+}
