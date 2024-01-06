d269e356c68463cf38a983726ba193d39276478a
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2024-01-06 00:32:31 +0000

    Add link to home page in about us page
    Co-authored-by: Christina V <christinamd95@hotmail.com>

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import Logo from "../ui/components/logo";
 
 export default function Page() {
   return (
-    <div className="md:absolute md:flex">
+    <Link href={"/"} className="md:absolute md:flex">
       <Logo />
-    </div>
+    </Link>
   );
 }
