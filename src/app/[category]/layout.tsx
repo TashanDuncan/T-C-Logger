f0c9b0c78cc7b07bce64dcdeb583938651ff034a
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2024-01-07 08:28:50 +0000

    Refactor layout component

--- src/app/[category]/layout.tsx
+++ src/app/[category]/layout.tsx
@@ -2,13 +2,11 @@ import SideNav from "@/app/ui/components/sidenav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-          <div className="w-full flex-none md:w-64 border-r-2  border-r-blue-600">
-            <SideNav />
-          </div>
-          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-            {children}
-          </div>
-        </div>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64 border-r-2  border-r-blue-600">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
   );
 }
