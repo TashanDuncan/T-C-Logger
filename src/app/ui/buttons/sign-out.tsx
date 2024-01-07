18c5aaa1a016f17dd2519e4b63fcecfdd8fc815b
viewtifult <tashanduncan1994@gmail.com>
2024-01-07 22:47:26 +0000

    Refactor SignOutButton component to use AlertDialog

--- src/app/ui/buttons/sign-out.tsx
+++ src/app/ui/buttons/sign-out.tsx
@@ -1,15 +1,42 @@
 import { signOut } from "@/auth";
 import { Button } from "../components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../components/ui/alert-dialog";
 
 export default function SignOutButton({ className }: { className?: string }) {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signOut();
-      }}
-    >
-      <Button className={className}>Sign Out</Button>
-    </form>
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button className={className}>Sign Out</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you absolutely sure you want to Sign Out?
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>No</AlertDialogCancel>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <AlertDialogAction type="submit">
+              Yes, Sign me out!
+            </AlertDialogAction>
+          </form>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
