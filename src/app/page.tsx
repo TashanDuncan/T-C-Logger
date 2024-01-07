4933f87a75ae89be7ac171e47b05dbd647b27514
viewtifult <tashanduncan1994@gmail.com>
2024-01-07 21:53:52 +0000

    Add mobile navigation component and reorganize layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,58 +13,20 @@ import { fetchItemCategories } from "./lib/data";
 import Image from "next/image";
 import Logo from "./ui/components/logo";
 import { ThemeToggle } from "./ui/components/theme-toggle";
+import { MobileNavigation } from "./ui/components/mobile-nav";
 
 export default async function Home() {
   const itemCategories = await fetchItemCategories();
   return (
-    <main className="flex min-h-screen justify-center items-center flex-col p-24">
-      <Logo />
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline">Menu</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuLabel>Our Favourites</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            {itemCategories &&
-              itemCategories.map((item, i) => (
-                <Link href={item.slug} key={i}>
-                  <DropdownMenuItem>{item.description}</DropdownMenuItem>
-                </Link>
-              ))}
-            <DropdownMenuSeparator />
-            <Link href={"/categories/new"}>
-              <DropdownMenuItem>Add New</DropdownMenuItem>
-            </Link>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
-
-      <div className="mt-auto w-full flex justify-between text-center">
-        <Link href={"/about-us"}>
-          <Button variant="outline">About Us</Button>
-        </Link>
-        <div className="flex justify-center">
-          <span className="inline-flex items-center mr-3">Our Playlist</span>
-          <a
-            href="https://open.spotify.com/playlist/6FsTeuxK1Y3jgAIxyVqtbE?si=98143e7825ed458f"
-            target="_blank"
-          >
-            <Button variant="outline">
-              <Image
-                src={"/spotifyicon.png"}
-                alt="Spotify Icon"
-                width={30}
-                height={30}
-              />
-            </Button>
-          </a>
+    <>
+      <div className="hidden md:block">
+        <MobileNavigation categories={itemCategories} />
+      </div>
+      <main className="flex justify-center items-center flex-col p-24">
+        <div className="md:absolute md:flex self-start">
+          <Logo />
         </div>
-      </div>
-      <div className="flex justify-center flex-col p-24">
-        <ThemeToggle />
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
