6932dd1436d4bda4aa22f9a277408a80f80a3503
viewtifult <tashanduncan1994@gmail.com>
2024-01-07 21:55:43 +0000

    Remove unused imports and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "./ui/components/ui/dropdown-menu";
-import { Button } from "./ui/components/ui/button";
-import Link from "next/link";
 import { fetchItemCategories } from "./lib/data";
-import Image from "next/image";
 import Logo from "./ui/components/logo";
-import { ThemeToggle } from "./ui/components/theme-toggle";
+
 import { MobileNavigation } from "./ui/components/mobile-nav";
 
 export default async function Home() {
