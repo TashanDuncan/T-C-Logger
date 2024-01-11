194f18c5034249d65b32412f9f9d0fcd06cf14c6
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2024-01-11 20:54:49 +0000

    Update main page layout

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
       <div className="hidden md:block">
         <MobileNavigation categories={itemCategories} />
       </div>
-      <main className="flex justify-center items-center flex-col px-24 md:p-24">
+      <main className="flex justify-center items-center flex-col px-12 md:px-24 md:p-24">
         <div className="md:absolute md:flex self-start">
           <Logo />
         </div>
